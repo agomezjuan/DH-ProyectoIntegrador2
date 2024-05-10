5210b14c371eb5898b94bba44a23915bb9d54312
LordOfCinder2 <matias.dilisa@gmail.com>
2024-05-10 11:41:46 -0300

    created project folders

---
+++ Backend/pi2/src/test/java/com/example/pi2/Pi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
