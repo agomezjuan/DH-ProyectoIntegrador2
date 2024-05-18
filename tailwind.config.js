9360f4a4e9fc56059c5e9fac8c85dc03ec0e9a78
Carmen Vargas <trishiavargas@gmail.com>
2024-05-17 22:34:46 -0300

    "Se configuró Tailwind"

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:{
+          DEFAULT:"#557824",
+          200:"#557824",
+          300:"#557824",
+          400:"#557824",
+
+        },
+        secondary:{
+          DEFAULT:{
+            42:"#557824",
+          }
+      },
+    },
+  },
+  plugins: [],
+},
+
