e12ddaff71883d287e8ba3f1427424e8fd0fbf4a
Andrea1789 <andreaagudelocardona789@gmail.com>
2024-05-27 12:58:10 -0500

    feat(frontend): :sparkles: create

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,12 +1,12 @@
 import axios from "../libs/axios";
 
 export const loginRequest = async (email, password) =>
-  axios.post("/api/auth/login", {
+  axios.post("http://localhost:8089/realms/proyecto-integrador/protocol/openid-connect/token", {
     email,
     password,
   });
 
 export const registerRequest = async (data) =>
-  axios.post("/api/auth/register", data);
+  axios.post("http://localhost:8090/api/v1/users/register", data);
 
 export const profileRequest = async () => axios.get("/api/auth/profile");
