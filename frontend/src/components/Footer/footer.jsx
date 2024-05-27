6f939bd102fa4b63c8830a65634d8551e4e556b5
Carmen Vargas <trishiavargas@gmail.com>
2024-05-27 00:39:39 -0300

    "fix: se arregla posición componente footer"

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import './footer.css';
 
-
-const transformFirstLetter = (text) => {
-  return text.split('').map((char, index) => {
-      if (index === text.search(/\w/)) {
-          return char.toUpperCase();
-      } else if (char.toLowerCase() === text.charAt(index)) {
-          return char.toLowerCase();
-      }
-      return char.toUpperCase();
-  }).join('');
-};
-
 const Footer = () => {
   return (
-    <footer className="footer w-full p-10 bg-primary text-primary-content">
-      <div className="flex flex-wrap justify-around">
-        <nav className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-          <h2 className="footer-title text-primary-content">Registrate</h2>
-          <a href="#" aria-label="X" className="text-neutral-300 hover:text-primary-content">
-          <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-  <path fill-rule="evenodd" d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z" clip-rule="evenodd"/>
-</svg>
-</a>
-          </nav>
-          </div>
+<footer className="footer w-full p-10 bg-primary text-primary-content">
+  <div className="flex items-center justify-center">
+    <nav className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2 flex justify-center">
+      <a href="#" aria-label="Registration" className="relative h-32 w-32 text-neutral-100 hover:text-primary-content flex items-center">
+        <svg className="w-6 h-6 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+          <path fillRule="evenodd" d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z" clipRule="evenodd"/>
+        </svg>
+        <span className="text-l text-neutral-100">Registrate</span>
+      </a>
+    </nav>
+  </div>
 <div id="verticle-line">
 
 </div>
