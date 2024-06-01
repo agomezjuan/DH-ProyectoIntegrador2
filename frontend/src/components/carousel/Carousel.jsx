0efac8b5bc9f663f7baa7a888a71eafe3c4ab5ad
Carmen Vargas <trishiavargas@gmail.com>
2024-06-01 01:48:17 -0300

    "fix: tamaño de imagenes"

--- frontend/src/components/carousel/Carousel.jsx
+++ frontend/src/components/carousel/Carousel.jsx
@@ -5,27 +5,28 @@ export const Carousel = () => {
   return (
     <div className="carousel rounded-box">
     <div className="carousel-item">
-      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Burger"className="w-72 h-72 rounded-full mx-auto" />
+      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Burger"className="w-60 h-60 rounded-full mx-auto" />
+    </div>
+
+    <div className="carousel-item">
+      <img src="\imagenes PI\Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg" alt="Burger"className="w-64 h-64 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg" alt="Burger"className="w-72 h-72 rounded-full mx-auto" />
+      <img src="\imagenes PI\Creamy-Turkey-Meatballs-Recipe.jpg" alt="Burger" className="w-64 h-64 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Creamy-Turkey-Meatballs-Recipe.jpg" alt="Burger" className="w-72 h-72 rounded-full mx-auto" />
+      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Burger" className="w-64 h-64 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Burger" className="w-72 h-72 rounded-full mx-auto" />
+      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Brocoli" className="w-64 h-64 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Brocoli" className="w-72 h-72 rounded-full mx-auto" />
+      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Chicken"className="w-64 h-64 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Chicken"className="w-72 h-72 rounded-full mx-auto" />
+      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Salad"className="w-64 h-64 rounded-full mx-auto" />
     </div>
-    <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Salad"className="w-72 h-72 rounded-full mx-auto" />
     </div>
-  </div>
 
     );
   };
