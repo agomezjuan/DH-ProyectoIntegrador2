789e47875768c55e6005f5434ce817307c180b65
Carmen Vargas <trishiavargas@gmail.com>
2024-05-31 21:50:38 -0300

    "Se cargan imágenes carrousel"

---
+++ frontend/src/components/carousel/Carousel.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Carousel = () => {
+  return (
+    <div className="carousel rounded-box">
+  <div className="carousel-item">
+    <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Burger"className="w-72 h-auto" />
+  </div>
+  <div className="carousel-item">
+    <img src="\imagenes PI\Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg" alt="Burger"className="w-72 h-auto" />
+  </div>
+  <div className="carousel-item">
+    <img src="\imagenes PI\Creamy-Turkey-Meatballs-Recipe.jpg" alt="Burger" className="w-72 h-auto" />
+  </div>
+  <div className="carousel-item">
+    <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Burger" className="w-72 h-auto" />
+  </div>
+  <div className="carousel-item">
+    <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Burger" className="w-72 h-auto" />
+  </div>
+  <div className="carousel-item">
+    <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Burger"className="w-72 h-auto" />
+  </div>
+  <div className="carousel-item">
+    <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Burger"className="w-72 h-auto" />
+  </div>
+</div>
+
+  );
+};
