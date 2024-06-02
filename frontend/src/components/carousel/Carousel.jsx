9b27d4ade06260a6ed55de71053b83bba56a41bf
Carmen Vargas <trishiavargas@gmail.com>
2024-06-02 01:53:45 -0300

    "Fix: se agrega chevron"

--- frontend/src/components/carousel/Carousel.jsx
+++ frontend/src/components/carousel/Carousel.jsx
@@ -1,33 +1,49 @@
 import React from "react";
 import './index.css';
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
 export const Carousel = () => {
   return (
+    <div class="carousel__contenedor relative">
+				<button aria-label="Anterior" className="carousel__anterior btn btn-white absolute top-32 transform -translate-y-1/2 -left-12">
+        <FaChevronLeft size={15} className="text-primary" />
+				</button>
+
     <div className="carousel rounded-box">
+      <div class="carousel">
+
     <div className="carousel-item">
-      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Burger"className="w-60 h-60 rounded-full mx-auto" />
+      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Burger"className="w-72 h-72 rounded-full mx-auto" />
     </div>
 
     <div className="carousel-item">
-      <img src="\imagenes PI\Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg" alt="Burger"className="w-64 h-64 rounded-full mx-auto" />
+      <img src="\imagenes PI\Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg" alt="Burger"className="w-72 h-72 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Creamy-Turkey-Meatballs-Recipe.jpg" alt="Burger" className="w-64 h-64 rounded-full mx-auto" />
+      <img src="\imagenes PI\Creamy-Turkey-Meatballs-Recipe.jpg" alt="Burger" className="w-72 h-72 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Burger" className="w-64 h-64 rounded-full mx-auto" />
+      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Burger" className="w-72 h-72 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Brocoli" className="w-64 h-64 rounded-full mx-auto" />
+      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Brocoli" className="w-72 h-72 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Chicken"className="w-64 h-64 rounded-full mx-auto" />
+      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Chicken"className="w-72 h-72 rounded-full mx-auto" />
     </div>
     <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Salad"className="w-64 h-64 rounded-full mx-auto" />
-    </div>
+      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Salad"className="w-72 h-72 rounded-full mx-auto" />
     </div>
 
+    </div>
+    </div>
+    <div>
+    <button aria-label="Siguiente" className="carousel__siguiente btn text-white absolute top-32 transform -translate-y-1/2 -right-12">
+    <FaChevronRight size={15} className="text-primary" />
+				</button>
+        </div>
+        </div>
     );
   };
 
