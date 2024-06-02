19fa5cb4cd619e53d2d0b3f92bed4267fd1e612d
Carmen Vargas <trishiavargas@gmail.com>
2024-06-02 17:58:57 -0300

    "fix: se agregan títulos y botones"

--- frontend/src/components/carousel/Carousel.jsx
+++ frontend/src/components/carousel/Carousel.jsx
@@ -2,53 +2,59 @@ import React, { useState } from "react";
 import './index.css';
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
+
 export const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const images = [
-    "/imagenes PI/Broccoli-Meal-Prep-Recipe.jpg",
-    "/imagenes PI/Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg",
-    "/imagenes PI/Creamy-Turkey-Meatballs-Recipe.jpg",
-    "/imagenes PI/Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg",
-    "/imagenes PI/Broccoli-Meal-Prep-Recipe.jpg",
-    "/imagenes PI/Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg",
-    "/imagenes PI/Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg"
+    {src:"/imagenes PI/Broccoli-Meal-Prep-Recipe.jpg",title: "30 minutos"},
+    {src:"/imagenes PI/Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg",title: "Aperitivos"},
+    {src:"/imagenes PI/postres.jpg",title: "Postres"},
+    {src:"/imagenes PI/Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg",title: "Plato Fuerte"},
   ];
 
   const handlePrevious = () => {
-    const newIndex = (currentIndex === 0) ? images.length - 4 : currentIndex - 1;
+    const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
   const handleNext = () => {
-    const newIndex = (currentIndex === images.length - 4) ? 0 : currentIndex + 1;
+    const newIndex = (currentIndex + 1) % images.length;
     setCurrentIndex(newIndex);
   };
+
+  const displayImages = [];
+  for (let i = 0; i < 4; i++) {
+    displayImages.push(images[(currentIndex + i) % images.length]);
+  }
 
   return (
     <div className="carousel__contenedor relative w-full">
       <button
         aria-label="Anterior"
-        className="carousel__anterior btn btn-circle btn-primary absolute top-1/2 transform -translate-y-1/2 left-4 z-10"
+        className="carousel__anterior btn-link bg-none absolute top-1/2 transform -translate-y-1/2 -left-7 z-10"
         onClick={handlePrevious}
       >
-        <FaChevronLeft size={30} />
+        <FaChevronLeft size={60}/>
       </button>
 
-      <div className="carousel flex overflow-hidden">
-        {images.slice(currentIndex, currentIndex + 4).map((image, index) => (
-          <div key={index} className="carousel-item flex-shrink-0 w-1/4 p-2">
-            <img src={image} alt={`Imagen ${index}`} className="w-full h-72 object-cover rounded-lg" />
+      <div className="carousel flex overflow-hidden justify-center">
+        {displayImages.map((image, index) => (
+          <div key={index} className="carousel-item flex flex-col items-center p-2 text-center">
+            <div className="w-60 h-60 overflow-hidden rounded-full mx-auto">
+              <img src={image.src} alt={`Imagen ${index}`} className="w-full h-full object-cover" />
+            </div>
+            <div className="text-xl font-semibold text-primary text-center">{image.title}</div>
           </div>
         ))}
       </div>
 
       <button
         aria-label="Siguiente"
-        className="carousel__siguiente btn btn-circle btn-primary absolute top-1/2 transform -translate-y-1/2 right-4 z-10"
+        className="carousel__siguiente btn-link bg-none absolute top-1/2 transform -translate-y-1/2 -right-7 z-10"
         onClick={handleNext}
       >
-        <FaChevronRight size={30} />
+        <FaChevronRight size={60} />
       </button>
     </div>
   );
