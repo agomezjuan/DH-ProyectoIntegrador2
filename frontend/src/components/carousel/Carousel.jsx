8725b2a57abc8ae5ebb211be5d3e3503c1076400
Carmen Vargas <trishiavargas@gmail.com>
2024-06-02 02:58:43 -0300

    "Fix: funcionalidad botón chevron"

--- frontend/src/components/carousel/Carousel.jsx
+++ frontend/src/components/carousel/Carousel.jsx
@@ -1,49 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import './index.css';
-import { FaChevronLeft } from "react-icons/fa";
-import { FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 export const Carousel = () => {
-  return (
-    <div class="carousel__contenedor relative">
-				<button aria-label="Anterior" className="carousel__anterior btn btn-white absolute top-32 transform -translate-y-1/2 -left-12">
-        <FaChevronLeft size={15} className="text-primary" />
-				</button>
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    <div className="carousel rounded-box">
-      <div class="carousel">
+  const images = [
+    "/imagenes PI/Broccoli-Meal-Prep-Recipe.jpg",
+    "/imagenes PI/Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg",
+    "/imagenes PI/Creamy-Turkey-Meatballs-Recipe.jpg",
+    "/imagenes PI/Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg",
+    "/imagenes PI/Broccoli-Meal-Prep-Recipe.jpg",
+    "/imagenes PI/Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg",
+    "/imagenes PI/Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg"
+  ];
 
-    <div className="carousel-item">
-      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Burger"className="w-72 h-72 rounded-full mx-auto" />
-    </div>
-
-    <div className="carousel-item">
-      <img src="\imagenes PI\Creamy-Parmesan-Green-Beans-Casserole-with-Bacon.jpg" alt="Burger"className="w-72 h-72 rounded-full mx-auto" />
-    </div>
-    <div className="carousel-item">
-      <img src="\imagenes PI\Creamy-Turkey-Meatballs-Recipe.jpg" alt="Burger" className="w-72 h-72 rounded-full mx-auto" />
-    </div>
-    <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Burger" className="w-72 h-72 rounded-full mx-auto" />
-    </div>
-    <div className="carousel-item">
-      <img src="\imagenes PI\Broccoli-Meal-Prep-Recipe.jpg" alt="Brocoli" className="w-72 h-72 rounded-full mx-auto" />
-    </div>
-    <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Chicken"className="w-72 h-72 rounded-full mx-auto" />
-    </div>
-    <div className="carousel-item">
-      <img src="\imagenes PI\Grilled-Lemon-Garlic-Chicken-Skewers-recipe.jpg" alt="Salad"className="w-72 h-72 rounded-full mx-auto" />
-    </div>
-
-    </div>
-    </div>
-    <div>
-    <button aria-label="Siguiente" className="carousel__siguiente btn text-white absolute top-32 transform -translate-y-1/2 -right-12">
-    <FaChevronRight size={15} className="text-primary" />
-				</button>
-        </div>
-        </div>
-    );
+  const handlePrevious = () => {
+    const newIndex = (currentIndex === 0) ? images.length - 4 : currentIndex - 1;
+    setCurrentIndex(newIndex);
   };
+
+  const handleNext = () => {
+    const newIndex = (currentIndex === images.length - 4) ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div className="carousel__contenedor relative w-full">
+      <button
+        aria-label="Anterior"
+        className="carousel__anterior btn btn-circle btn-primary absolute top-1/2 transform -translate-y-1/2 left-4 z-10"
+        onClick={handlePrevious}
+      >
+        <FaChevronLeft size={30} />
+      </button>
+
+      <div className="carousel flex overflow-hidden">
+        {images.slice(currentIndex, currentIndex + 4).map((image, index) => (
+          <div key={index} className="carousel-item flex-shrink-0 w-1/4 p-2">
+            <img src={image} alt={`Imagen ${index}`} className="w-full h-72 object-cover rounded-lg" />
+          </div>
+        ))}
+      </div>
+
+      <button
+        aria-label="Siguiente"
+        className="carousel__siguiente btn btn-circle btn-primary absolute top-1/2 transform -translate-y-1/2 right-4 z-10"
+        onClick={handleNext}
+      >
+        <FaChevronRight size={30} />
+      </button>
+    </div>
+  );
+};
 
