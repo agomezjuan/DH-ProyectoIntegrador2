3e5634f32cc97e5198a142300d801091511e98ed
Carmen Vargas <85655835+CarmenVar@users.noreply.github.com>
2024-06-03 07:00:41 -0300

    20 componente carousel (#49)
    * "Se cargan imágenes carrousel"
    
    * "fix: se arreglan imagenes "
    
    * "fix: tamaño de imagenes"
    
    * "Fix: se agrega chevron"
    
    * "Fix: funcionalidad botón chevron"
    
    * "fix: se agregan títulos y botones"

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import { Layout } from '@/components/Layout';
 import { Header } from '@/components/Header';
 import { RecipeContainer } from '@/components/RecipeContainer';
 import { recipeMock } from '@/mocks/recipeMock';
+import { Carousel } from '../../components/carousel/Carousel';
+
 
 const popularRecipes = [recipeMock, recipeMock, recipeMock];
 const newRecipes = [recipeMock, recipeMock, recipeMock];
@@ -13,8 +15,12 @@ export const Home = () => {
       <div className='container bg-base-200 p-6'>
         <Header />
         <div className='mt-11'></div>
+        <div className='container bg-base-200 p-6'>
+      <Carousel/>
         <RecipeContainer title='Popular Recipes' recipes={popularRecipes} />
         <RecipeContainer title='New Recipes' recipes={newRecipes} />
+
+   </div>
       </div>
     </Layout>
   );
