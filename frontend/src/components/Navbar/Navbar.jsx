54d8b47d61872f2921521dbae97f913f12b22c1d
Carmen Vargas <trishiavargas@gmail.com>
2024-06-06 00:33:23 -0300

    "fix: se arregla typo footer x Footer file index.js"

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ export default function Navbar() {
           navigate("/");
         }
         setToastMessage(null);
-      }, 5000); 
+      }, 5000);
     }
     return () => clearTimeout(timer);
   }, [toastMessage, navigate]);
@@ -41,20 +41,6 @@ export default function Navbar() {
   return (
     <div className='navbar bg-base-100'>
       <div className='flex-none'>
-        <button className='btn btn-square btn-ghost text-primary'>
-         
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            className='inline-block w-5 h-5 stroke-current'>
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M4 6h16M4 12h16M4 18h16'></path>
-          </svg>
-        </button>
       </div>
       <div className='flex-1'>
       <Link to='/'>
@@ -69,7 +55,7 @@ export default function Navbar() {
             </summary>
           <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
           {
-            !isAuth ?  
+            !isAuth ?
             <>
             <li><Link to='/login' >Login</Link></li>
             <li><Link to='/register' >Registro</Link></li>
