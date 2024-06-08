a088bf7b49034a1afd67461690ce9c4eface8c59
LordOfCinder2 <matias.dilisa@gmail.com>
2024-06-08 19:12:18 -0300

    fixed get url

--- frontend/src/api/recipes.js
+++ frontend/src/api/recipes.js
@@ -1,8 +1,20 @@
-import httpService from './httpService';
+// import httpService from './httpService';
+
+// export const getRecipes = async () => {
+//   try {
+//     const response = await httpService.get('/recipes');
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching recipes:', error);
+//     throw error;
+//   }
+// };
+
+import axios from 'axios';
 
 export const getRecipes = async () => {
   try {
-    const response = await httpService.get('/recipes');
+    const response = await axios.get('http://localhost:8090/api/v1/recipes');
     return response.data;
   } catch (error) {
     console.error('Error fetching recipes:', error);
