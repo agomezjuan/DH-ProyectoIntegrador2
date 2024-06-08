686c2c820e82cd234ae911705b56e4fb4715c005
Carmen Vargas <trishiavargas@gmail.com>
2024-06-08 15:11:19 -0300

    "Se modifican estilos"

--- frontend/src/pages/Planner/Planner.jsx
+++ frontend/src/pages/Planner/Planner.jsx
@@ -71,7 +71,7 @@ const Planner = () => {
                                     />
                                 </td>
                                 <td>
-                                    <button onClick={() => alert('Editar fechas para ' + day.name)}>Editar</button>
+                                    <button onClick={() => alert('Editar fechas para ' + day.name)}><svg class="h-6 w-6 text-primary"  viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />  <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />  <line x1="16" y1="5" x2="19" y2="8" /></svg></button>
                                 </td>
                             </tr>
                         ))}
@@ -85,6 +85,7 @@ const Planner = () => {
             </div>
         </div>
         </div>
+
     );
 };
 
