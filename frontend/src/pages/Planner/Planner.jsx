c9eeb8f78b5ce2635ee44b8f9190609504a93b5d
Carmen Vargas <trishiavargas@gmail.com>
2024-06-08 10:38:10 -0300

    "arreglando estilos"

--- frontend/src/pages/Planner/Planner.jsx
+++ frontend/src/pages/Planner/Planner.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import knifeplateImage from './knifeplate.png';
 import './Planner.css';
 
 const Planner = () => {
@@ -33,13 +34,30 @@ const Planner = () => {
         <div className="planner-container">
             <h1>AGENDA SEMANAL</h1>
             <div className="planner">
+                <div className="header-titles">
+                    <h2>Día</h2>
+                    <div className="header-recipes">
+                    <img src={knifeplateImage} alt="Knife Plate"style={{ width: '24px', height: '24px', colorClass:"#82AA33" }} />
+                        <h2>Recetas</h2>
+                    </div>
+                    <div className="header-edit-dates">
+
+                        <svg className="h-4 w-4 text-primary" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z"/>
+                            <rect x="4" y="5" width="16" height="16" rx="2" />
+                            <line x1="16" y1="3" x2="16" y2="7" />
+                            <line x1="8" y1="3" x2="8" y2="7" />
+                            <line x1="4" y1="11" x2="20" y2="11" />
+                            <rect x="8" y="15" width="2" height="2"/>
+
+                        </svg>
+                        <h2>Editar Fechas</h2>
+                    </div>
+                </div>
+
                 <table>
                     <thead>
-                        <tr>
-                            <th>Día</th>
-                            <th>Recetas</th>
-                            <th>Editar Fechas</th>
-                        </tr>
+                        <tr></tr>
                     </thead>
                     <tbody>
                         {daysOfWeek.map((day, index) => (
@@ -60,6 +78,7 @@ const Planner = () => {
                         ))}
                     </tbody>
                 </table>
+
                 <div className="planner-buttons">
                     <button onClick={handleDownload}>Descargar Planner</button>
                     <button onClick={handleContinue}>Continuar Agendando</button>
