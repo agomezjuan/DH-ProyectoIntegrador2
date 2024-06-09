68c3bb5997a114515573bef39b8e528e979a5b09
Andrea1789 <andreaagudelocardona789@gmail.com>
2024-06-09 11:36:52 -0500

    content

--- frontend/src/api/recipes.js
+++ frontend/src/api/recipes.js
@@ -3,7 +3,7 @@ import httpService from './httpService';
 export const getRecipes = async () => {
   try {
     const response = await httpService.get('/api/v1/recipes/pagination');
-    return response.data;
+    return response.content;
   } catch (error) {
     console.error('Error fetching recipes:', error);
     throw error;
