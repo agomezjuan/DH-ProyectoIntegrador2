254e23dc859423cf2a7b93f34169c392294befad
Andrea1789 <andreaagudelocardona789@gmail.com>
2024-06-09 14:03:48 -0500

    detail

--- frontend/src/api/recipes.js
+++ frontend/src/api/recipes.js
@@ -13,7 +13,7 @@ export const getRecipes = async () => {
 export const getRecipeById = async (id) => {
   try {
     const response = await httpService.get(`/api/v1/recipes/${id}`);
-    return response;
+    return response.data;
   } catch (error) {
     console.error('Error fetching recipes:', error);
     throw error;
