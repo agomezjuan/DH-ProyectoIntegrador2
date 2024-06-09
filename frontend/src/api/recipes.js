658cdda9e2328bbf7f9e2d5bc14b7bbd9de6154a
LordOfCinder2 <matias.dilisa@gmail.com>
2024-06-09 12:23:09 -0300

    fixed .env file

--- frontend/src/api/recipes.js
+++ frontend/src/api/recipes.js
@@ -1,20 +1,8 @@
-// import httpService from './httpService';
-
-// export const getRecipes = async () => {
-//   try {
-//     const response = await httpService.get('/recipes');
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error fetching recipes:', error);
-//     throw error;
-//   }
-// };
-
-import axios from 'axios';
+import httpService from './httpService';
 
 export const getRecipes = async () => {
   try {
-    const response = await axios.get('http://localhost:8090/api/v1/recipes');
+    const response = await httpService.get('/api/v1/recipes/pagination');
     return response.data;
   } catch (error) {
     console.error('Error fetching recipes:', error);
