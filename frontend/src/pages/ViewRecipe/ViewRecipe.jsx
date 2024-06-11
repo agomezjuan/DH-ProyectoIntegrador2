b2a16029447d13fcde3d7ed4226ead3270e03f3e
Matias Di Lisa <90879673+LordOfCinder2@users.noreply.github.com>
2024-06-11 18:53:00 -0300

    Front search function (#85)
    * added call to backend
    
    * added SearchBar component to ViewRecipe

--- frontend/src/pages/ViewRecipe/ViewRecipe.jsx
+++ frontend/src/pages/ViewRecipe/ViewRecipe.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { Layout } from '@/components/Layout';
 import MainRecipe from '../../components/MainRecipe/MainRecipe';
-import { useRecipesStore } from '@/store/recipesStore';
+import { SearchBar } from '@/components/SearchBar/SearchBar';
+import { useRecipesStore } from '@/store/recipesStore'
 import { useParams } from 'react-router-dom';
 
 export const ViewRecipe = () => {
@@ -17,24 +18,29 @@ export const ViewRecipe = () => {
   return (
     <Layout>
       <div className='container bg-base-200 p-6'>
-        <div className='flex items-center justify-center p-8 text-center h-[200px] mx-20 bg-secondary'>
-          <input
-            type='text'
-            placeholder='Busca una receta, un ingrediente, palabra clave'
-            className='w-full p-4 rounded'
-            style={{ margin: '0 50px' }}
-          />
+        <div
+          style={{
+            backgroundColor: 'rgb(130, 170, 51)',
+            height: '200px',
+            margin: '0 80px'
+          }}
+          className='flex items-center justify-center p-8 text-center'>
+          <SearchBar />
         </div>
         <MainRecipe
           title={detail.recipe?.name}
           time={detail.recipe?.preparationTime}
           img={detail.recipe?.urlImg}
         />
+        <div style={{ marginLeft: '80px', marginRight: '80px' }}>
         <div className='container mx-auto'>
           {loading && <p>Cargando...</p>}
           {error && <p>Error: {error}</p>}
           {!loading && !error && (
             <div>
+              <div className='my-5'>
+                <h2 style={{ fontWeight: 'bold' }}>Ingredientes</h2>
+                <ul>
               <div className='my-5 p-4 mx-20'>
                 <h2 className='text-xl font-bold mb-3'>Ingredientes</h2>
                 <ul className='list-disc px-5'>
@@ -44,6 +50,13 @@ export const ViewRecipe = () => {
                     ))}
                 </ul>
               </div>
+              <main className='p-4'>
+                <h1
+                  className='font-bold text-xl'
+                  style={{ marginTop: '20px', marginBottom: '20px' }}>
+                  Preparación
+                </h1>
+                <ul className='list-disc pl-5'>
               <main className='p-4 mx-20'>
                 <h1 className='font-bold text-xl mb-3'>Preparación</h1>
                 <ol className='pl-5 list-decimal'>
@@ -51,6 +64,7 @@ export const ViewRecipe = () => {
                     detail.recipe?.preparationSteps.map((step, index) => (
                       <li key={index}>{step}</li>
                     ))}
+                </ul>
                 </ol>
               </main>
             </div>
