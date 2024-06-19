cd1ef437a719fe5e63916de4c8dbf54f01b569d1
LordOfCinder2 <matias.dilisa@gmail.com>
2024-06-19 20:37:45 -0300

    fixed response csv, added deleteByUserId endpoint

--- Backend/pi2/src/main/java/com/example/pi2/controller/PlannerController.java
+++ Backend/pi2/src/main/java/com/example/pi2/controller/PlannerController.java
@@ -1,6 +1,7 @@
 package com.example.pi2.controller;
 
 import com.example.pi2.domain.PlannerDTO;
+import com.example.pi2.domain.PlannerDtoToCsv;
 import com.example.pi2.model.Planner;
 import com.example.pi2.service.PlannerService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -54,10 +55,10 @@ public class PlannerController {
 			return plannerService.saveForUser(idUser, planner);
 	  }
 
-	  @GetMapping("/getcsv")
+	  @GetMapping("/getcsvv2")
 	  public void sendCSV(HttpServletResponse response, @RequestParam String idUser) throws IOException {
 
-			String csvFileName = "Planner.csv";
+			String csvFileName = "PlanSemanal.csv";
 			response.setContentType("text/csv");
 			String headerKey = "Content-Disposition";
 			String headerValue = String.format("attachment; filename=\"%s\"",
@@ -75,10 +76,38 @@ public class PlannerController {
 			csvBeanWriter.close();
 	  }
 
+	  @GetMapping("/getcsv")
+	  public void sendCSVv2(HttpServletResponse response, @RequestParam String idUser) throws IOException {
+
+			String csvFileName = "PlanSemanal.csv";
+			response.setContentType("text/csv");
+			String headerKey = "Content-Disposition";
+			String headerValue = String.format("attachment; filename=\"%s\"",
+					csvFileName);
+			response.setHeader(headerKey, headerValue);
+			ICsvBeanWriter csvBeanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+			String[] header = {"dayOfTheWeek", "name", "description", "preparationTime", "preparationSteps"};
+			csvBeanWriter.writeHeader(header);
+			List<PlannerDtoToCsv> plannerDTOList = plannerService.dtoToCsv2(idUser);
+			for (PlannerDtoToCsv plannerDTO1 :
+					plannerDTOList) {
+				  csvBeanWriter.write(plannerDTO1, header);
+			}
+
+			csvBeanWriter.close();
+	  }
+
 	  @DeleteMapping("/{id}")
 	  public void delete(@PathVariable Long id) {
 
 			plannerService.deleteById(id);
+	  }
+
+	  @DeleteMapping("/byUserId")
+	  public void deleteByUserId(@AuthenticationPrincipal Jwt jwt) {
+
+			String userId = jwt.getSubject();
+			plannerService.deleteByUserId(userId);
 	  }
 
 }
