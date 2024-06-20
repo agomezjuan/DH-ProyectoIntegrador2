18ac4ab48532f60d9f85b65a93c8a49186459400
Andrea1789 <andreaagudelocardona789@gmail.com>
2024-06-20 17:59:36 -0500

    adding form

---
+++ frontend/src/components/UpdateUserData/UpdateUserData.jsx
@@ -0,0 +1,49 @@
+function UpdateUserData(){
+    return(
+        <div className='flex justify-around bg-zinc-200 bg-opacity-80 py-16 rounded-md'>
+
+        <form onSubmit={onSubmit}>
+        <div className='text-center'>
+          <h2 className='text-lg font-bold text-primary max-w-lg'>
+            Datos de Usuario
+          </h2>
+        </div>
+        <div className='flex  text-left'>
+          <input
+            className='w-80 mt-7 p-1 italic rounded-sm border border-solid border-primary focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary'
+            placeholder='Nombre'
+            type='text'
+            {...register('firstName')}
+          />
+          {errors.firstName && (
+            <span className='text-xs text-primary font-bold'>
+              {errors.firstName.message}
+            </span>
+          )}
+        </div>
+
+        <div className='mt-1 flex flex-col text-left'>
+          <input
+            className='w-80 mt-2 p-1 italic rounded-sm border border-solid border-primary focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary'
+            placeholder='Apellido'
+            type='text'
+            {...register('lastName')}
+          />
+          {errors.lastName && (
+            <span className='text-xs text-primary font-bold'>
+              {errors.lastName.message}
+            </span>
+          )}
+        </div>
+        <button
+            type='submit'
+            className='bg-primary px-3 font-semibold text-white p-2 mt-8 rounded-sm border border-solid border-primary hover:bg-green-900'>
+            Actualiza tus datos
+          </button>
+        </form>
+        </div>
+        
+    )
+}
+
+export default UpdateUserData;
---
+++ frontend/src/components/UpdateUserData/index.js
@@ -0,0 +1,1 @@
+export {default UpdateUserData} from './UpdateUserData';
