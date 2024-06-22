7dff74ff06332602eb118d4d06d46ccf80e34892
Maria Alejandra Pizarro Sarria <alejapizasa@gmail.com>
2024-06-21 19:09:11 -0500

    slogan

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
       <div className='absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-25 p-5'>
         <SearchBar />
         <div className='mt-8 bg-gray-200 p-6 rounded-md shadow-md text-center absolute bottom-[-70px] h-40'>
-          <p className='text-primary font-semibold mb-2'>Nueva Receta</p>
+          <p className='text-primary font-semibold mb-2'>Meal Map</p>
           <h1 className='text-3xl font-bold text-gray-900 max-w-lg'>
-            Receta de Pizza integral con levadura seca
+          Recetas rápidas para almuerzos perfectos
           </h1>
         </div>
       </div>
