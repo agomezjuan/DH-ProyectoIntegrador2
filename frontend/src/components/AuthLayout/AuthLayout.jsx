fbdf886635d0cfc74e98b6fddda7621fa768cec9
Alberto Gómez Juan <agomezjuan@hotmail.com>
2024-06-25 01:11:27 +0200

    fix auth container

--- frontend/src/components/AuthLayout/AuthLayout.jsx
+++ frontend/src/components/AuthLayout/AuthLayout.jsx
@@ -8,7 +8,7 @@ export default function AuthLayout({ children }) {
       <div className='relative w-full h-96'>
         <img src={hero} alt='Recetas' className='w-full h-full object-cover' />
       </div>
-      <div className='bg-white flex flex-col justify-items-center'>
+      <div className='bg-white flex flex-col justify-items-center container'>
         <div className='w-1/2 mx-auto -mt-40 mb-10 z-10'>{children}</div>
       </div>
     </Layout>
