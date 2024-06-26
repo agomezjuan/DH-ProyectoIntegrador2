a22d57261688992517f4ee0687f18c725d3b6ae7
Alberto Gómez Juan <agomezjuan@hotmail.com>
2024-06-26 22:42:49 +0200

    prohibir acceso no autorizado

--- frontend/src/pages/UserProfile/UserProfile.jsx
+++ frontend/src/pages/UserProfile/UserProfile.jsx
@@ -5,17 +5,24 @@ import { PlannerDnD } from '../Planner';
 import { useUserProfileStore } from '@/store/userProfileStore';
 import { useAuthStore } from '@/store/authStore';
 import { SearchBar } from '@/components/SearchBar/SearchBar';
+import { useNavigate } from 'react-router-dom';
 
 function UserProfile() {
   const { favoriteRecipes, fetchFavoriteRecipes, loading } =
     useUserProfileStore();
+  const isAuth = useAuthStore((state) => state.isAuth);
   const { profile, token } = useAuthStore();
+  const navigate = useNavigate();
 
   const username = profile?.email;
 
   useEffect(() => {
-    fetchFavoriteRecipes(token, username);
-  }, [fetchFavoriteRecipes, token, username]);
+    if (isAuth) {
+      fetchFavoriteRecipes(token, username);
+    } else {
+      navigate('/login');
+    }
+  }, [fetchFavoriteRecipes, isAuth, navigate, token, username]);
 
   return (
     <Layout>
